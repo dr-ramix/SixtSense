b8ab77ec586cb121172a8efaf2a769c2e3151935
Seyed Ramtin Hosseini <ramtin.hosseini1382@gmail.com>
2025-11-22 10:04:11 +0100

    Initial commit

---
+++ SixtSense/frontend/web/src/pages/remoteController/RemoteController.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./remoteController.css"
+
+
+const RemoteController = () => {
+    return (
+        <div className='pagesRemoteController'>
+                pagesRemoteController
+        </div>
+    );
+}
+
+export default RemoteController;
